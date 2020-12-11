36bc289b6b5dbe68b8a079da41883d76b89bf766
Denisse Santana <ledennii@gmail.com>
2020-12-11 11:34:15 -0600

    Uppload

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,85 @@
 const cipher = {
   // ...
+//    encode( offset, string)
+//  {
+//     let result = "";
+//     const abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+ 
+//     offset = (offset % 26 + 26) % 26;
+ 
+//     if (string!=null) {
+//                  //rotación de cadena, si no esta en el array se escribe tal cual
+//                 for (let i = 0; i<string.length; i++){
+//                   if (abc.indexOf(string[i])!=-1){
+//                   let position = ((abc.indexOf(string[i])+offset)%26);
+//                    result += abc[position]
+//                   }
+//                   else
+//                   result += string[i];
+//                   }                
+//                 return result;
+//                 }
+    //  else if (!string) {
+    //   throw new TypeError("Not valid");
+    //  }
+    //  else if (string === 0) {
+    //   throw new TypeError("Not valid");
+    //  }
+    //  else if (string === (null, [])) {
+    //   throw new TypeError("Not valid");
+    //  }
+    //  else if (string === (0,0)) {
+    //   throw new TypeError("Not valid");
+    //  }
+  // },
+
+   encode (offset, string)   
+   {
+    //  if (!string){
+    //               return "";
+    //              }
+    if (!string) {
+      throw new TypeError("Not valid");
+     }
+     else if (string === 0) {
+      throw new TypeError("Not valid");
+     }
+     else if (string === (null, [])) {
+      throw new TypeError("Not valid");
+     }
+     else if (string === (0,0)) {
+      throw new TypeError("Not valid");
+     }
+     const abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+     offset = (offset % 26 + 26) % 26;
+     return string.replace(/[A-Z]/ig, e=> abc[(abc.indexOf(e)+offset)%26]);
+   },
+ 
+   decode(offset, string){
+    //  if (!string){
+    //      return "";
+    //  }
+    //  if (!string) {
+    //   throw new TypeError("User input no puede ser una string vacio");
+    // }
+     if (!string) {
+      throw new TypeError("Not valid");
+     }
+     else if (string === 0) {
+      throw new TypeError("Not valid");
+     }
+     else if (string === (null, [])) {
+      throw new TypeError("Not valid");
+     }
+     else if (string === (0,0)) {
+      throw new TypeError("Not valid");
+     }
+    
+     const abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+     offset = (offset % 26 - 26) % 26;
+     return string.replace(/[A-Z]/ig, e=> abc[(abc.indexOf(e)-offset)%26]);
+   }  
+
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+window.addEventListener("load", start, true);
+
+function start(){
+
+// document.getElementById("messageIn").addEventListener("keyup",function(){
+//  this.value = this.value.toUpperCase();
+//  }, true);
+  // alert('Only English!')
+
+  document.getElementById('messageIn').addEventListener("keyup", function(){
+    let up = document.getElementById("messageIn");
+    up.value = up.value.toUpperCase();
+  }, true);
+
+  document.getElementById('forYou').addEventListener("click",function(){
+    let string = document.getElementById('messageIn').value;
+    let offset = document.getElementById('offset').value;
+    document.getElementById('messageOut').value = cipher.encode(offset,string);
+  }, true);
+
+  document.getElementById('toMe').addEventListener("click",function(){
+    let string = document.getElementById('messageIn').value;
+    let offset = document.getElementById('offset').value;
+    document.getElementById('messageOut').value = cipher.decode(offset,string);
+  }, true);
+}
+
+// var myWindow;
+
+// function openWin{
+//   myWindow = window.open("", "myWindow", "width=200,height=100");
+//   myWindow.document.write("<p>Esta es mi ventana'</p>");
+// }
+
+// function closeWin{
+//   myWindow.close();
+// }
